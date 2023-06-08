10fe0448d9f6acdd29bc1b60c74c5427610e5b29
Brayan <brayan-13sc@hotmail.com>
2023-06-08 16:57:55 -0600

    login y registro
    Fuaaaap

---
+++ Proyecto2/login.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+
+include 'conexion.php';
+
+$correo = $_POST['correo'];
+$contrasena = $_POST['contrasena'];
+
+$validarLogin = mysqli_query($con, "SELECT rol, contrasena FROM registro WHERE correo = '$correo'");
+$resultado = mysqli_fetch_assoc($validarLogin);
+
+if ($resultado) {
+    // Verificar la contraseña utilizando otro método si no está cifrada con password_hash()
+    if ($contrasena === $resultado['contrasena']) {
+        $_SESSION['usuario'] = $resultado;
+
+        // Redirigir al usuario según su rol
+        if ($resultado['rol'] == 1) {
+            header('Location: home1.html');
+        } elseif ($resultado['rol'] == 2) {
+            header('Location: home2.php');
+        } else {
+            // Rol desconocido
+            echo "Rol desconocido.";
+        }
+    } else {
+        // La contraseña es incorrecta
+        echo "Contraseña incorrecta.";
+    }
+} else {
+    // El correo no está registrado
+    echo "El correo no está registrado.";
+}
+
+mysqli_close($con);
+?>
