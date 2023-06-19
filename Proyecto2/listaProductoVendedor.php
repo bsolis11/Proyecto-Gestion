844654c01fe745429a7687516209b71cb89f0859
Elena210398 <gabyelena2103@gmail.com>
2023-06-18 20:03:53 -0600

    Update listaProductoVendedor.php

--- Proyecto2/listaProductoVendedor.php
+++ Proyecto2/listaProductoVendedor.php
@@ -1,10 +1,10 @@
 
-
-<!DOCTYPE html>
 <?php
     include 'conexion.php';
 
 ?>
+<!DOCTYPE html>
+
 <html lang="en">
 
 <head>
@@ -14,51 +14,56 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS v5.2.1 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/estilos.css">
+    <link rel="stylesheet" href="assets/css/estilosPrincipal.css">
 </head>
 
 <body class="bg-form-add-prod">
-  <header>
+<header>
     <!-- place navbar here -->
     <nav class="navbar navbar-expand-lg navbar-dark navbar-transparent">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="#">
-                <img src="ImagenesIndex/eco.png" alt="" width="70" height="54" class="d-inline-block align-text-top">
-                <h1>Vida</h1>
-              </a>
-      
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li class="nav-item me-5">
-                    <a class="nav-link active" aria-current="page" href="Principal1.html">
-                        <h5>Inicio</h5>
-                    </a>
-                  </li>
-                  <li class="nav-item me-5">
-                    <a class="nav-link active" href="../Proyecto2/recetas.html">
-                      <h5>Recetas</h5>
-                    </a>
-                  </li>
-                  <li class="nav-item me-5">
-                    <a class="nav-link active" aria-current="page" href="../Proyecto2/listaProductos.php">
-                        <h5>Tienda</h5> </a>
-                  </li>
-                  <li class="nav-item me-5">
-                    <a class="nav-link active" aria-current="page" href="../Proyecto2/index.html">
-                        <h5>Cerrar Sesión</h5> </a>
-                  </li>
-                  
-      
-              </div>
-            </div>
-          </nav>
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src="ImagenesIndex/eco.png" alt="" width="70" height="54" class="d-inline-block align-text-top">
+          <h1>Vida</h1>
+        </a>
 
-         
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item me-5">
+              <a class="nav-link active" aria-current="page" href="Principal2.html">
+                  <h5>Inicio</h5>
+              </a>
+            </li>
+            <li class="nav-item me-5">
+              <a class="nav-link active" href="../Proyecto2/CuidadoProductos.html">
+                <h5>Guía</h5>
+              </a>
+            </li>
+            <li class="nav-item me-5 dropdown letras ">
+              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><h5>Productos</h5></a>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="../Proyecto2/eliminarProducto.html">Eliminar un producto</a>
+                <a class="dropdown-item" href="../Proyecto2/modificarProducto.html">Editar productos</a>
+                <a class="dropdown-item" href="../Proyecto2/insertarProducto.html">Agregar productos</a>
+                <a class="dropdown-item" href="../Proyecto2/listaProductoVendedor.php">Lista de productos</a>
+                
+              </div>
+            </li>
+            <li class="nav-item me-5">
+              <a class="nav-link active" aria-current="page" href="../Proyecto2/index.html">
+                  <h5>Cerrar Sesión</h5> </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </header>
   <main>
     <div class="formulario-container">
@@ -101,17 +106,21 @@
                           <th> <?php echo $mostrar['cantidad']?> </th>
                           <th> <?php echo $mostrar['idVendedor']?> </th>
                           
-                          <th> <a name="" id="" class="btn btn-warning" href="../Proyecto2/modificarProducto.html" role="button">Editar</a>
-                               <a name="" id="" class="btn btn-danger" href="../Proyecto2/eliminarProductoVista.php?nombreProducto=<?php echo urlencode($mostrar['nombreProducto']); ?>" role="button">Borrar</a> 
+                          <th> <a name="" id="" class="btn btn-outline-warning" 
+                          href="../Proyecto2/modificarProductoVista.php?nombreProducto=<?php echo urlencode($mostrar['nombreProducto']); 
+                           ?>&nombreVendedor=<?php echo urlencode($mostrar['nombreVendedor']); 
+                           ?>&precio=<?php echo urlencode($mostrar['precio']);
+                           ?>&contacto=<?php echo urlencode($mostrar['contacto']);
+                           ?>&descripcion=<?php echo urlencode($mostrar['descripcion']);
+                           ?>&cantidad=<?php echo urlencode($mostrar['cantidad']);?>" role="button">Editar</a>
+                               <a name="" id="" class="btn btn-outline-danger" href="../Proyecto2/eliminarProductoVista.php?nombreProducto=<?php echo urlencode($mostrar['nombreProducto']); ?>" role="button">Borrar</a> 
                           </th>
-                          <th>
-        </th>
                           
                       </tr>
-             <?php   
-                    
+                      <?php   
+                          }  
                        
-             ?>
+                      ?>
                 </table>            
             </div>
         </div>
